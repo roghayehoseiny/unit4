25251e8e015c9b53ced2f508da9480d0d2609cc6
roghayehoseiny <roghayehoseiny.2003@gmail.com>
2024-11-03 13:43:34 +0100

    lcd

---
+++ lcd-helloworld.ino
@@ -0,0 +1,16 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12,11,5,4,3,2);
+void setup() {
+  lcd.begin(16,2);
+  lcd.clear();
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  lcd.print("Hello world");
+  lcd.setCursor(0,1);
+  lcd.print("LCD Tutorial");
+  // put your main code here, to run repeatedly:
+
+}
